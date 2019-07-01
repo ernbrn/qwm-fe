769eca651a6b292d846563254f96ef3f9df6faa8
Erin Brown <emberbrown@gmail.com>
2019-06-30 22:26:16 -0400

    Move index code into app code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import CenteredAppContainer from 'shared/CenteredAppContainer';
 import { Typography } from '@material-ui/core';
-
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from 'configureStore';
 
 export default function App() {
   return (
-    <CenteredAppContainer>
-      <Typography variant="h2" component="h1" align="center">
-        Hello World!
-      </Typography>
-    </CenteredAppContainer>
+    <Provider store={configureStore()}>
+      <ConnectedRouter history={history}>
+        <CenteredAppContainer>
+          <Typography variant="h2" component="h1" align="center">
+            Hello World!
+          </Typography>
+        </CenteredAppContainer>
+      </ConnectedRouter>
+    </Provider>
   );
 }
