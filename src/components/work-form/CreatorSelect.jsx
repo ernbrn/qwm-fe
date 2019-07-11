6ea3a2baaee6648051b4a8d20f08aad09b5de261
Erin Brown <emberbrown@gmail.com>
2019-07-10 23:51:35 -0400

    Some good clean up

--- src/components/work-form/CreatorSelect.jsx
+++ src/components/work-form/CreatorSelect.jsx
@@ -5,21 +5,50 @@ import {
 } from '@material-ui/core';
 
 export default function CreatorSelect({ input, meta, placeholder }) {
-  const NO_RESULTS = 'Add creator';
+  const NO_RESULTS = 'addCreator';
 
   const itemToString = item => item || '';
+  const noResultsItem = { id: NO_RESULTS, name: 'Add new creator' };
   const [items, setItems] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const { onChange, ...restInput } = input;
 
   function fakePI(inputValue) {
+    // TODO: debounce
     return new Promise(resolve => setTimeout(() => {
-      setItems([{ id: 1, name: 'Sarah Waters' }, { id: 2, name: 'Someone else' }]);
+      setItems([{ id: 1, name: 'Sarah Waters' }, noResultsItem]);
       return resolve;
     }, 300));
   }
 
-  const { onChange, ...restInput } = input;
+  function handleSelection(selectValue) {
+    setInputValue('');
+
+    if (selectValue.id === NO_RESULTS) {
+      console.log('here');
+      // set some state to add a creator input or something
+      // or pop a modal open????
+      return;
+    }
+
+    setSelectedItems((prevState) => {
+      // remove the duplicate if the incoming one is a dupe
+      // there's gotta be a better way to do this
+      const allSelected = [...prevState.filter(i => i.id !== selectValue.id), selectValue];
+      onChange(allSelected);
+
+      return allSelected;
+    });
+  }
+
+  const handleDelete = selectedCreatorId => () => {
+    setSelectedItems((prevState) => {
+      const newSelectedItems = prevState.filter(i => i.id !== selectedCreatorId);
+      onChange(newSelectedItems);
+      return newSelectedItems;
+    });
+  };
 
   return (
     <Downshift
@@ -34,14 +63,11 @@ export default function CreatorSelect({ input, meta, placeholder }) {
       itemToString={itemToString}
       selectedItem={input.value}
       onSelect={(selectValue) => {
+        // check for selectValue
+        // if you press esc when the menu is open
+        // selectValue will be undefined and break EVERYTHING
         if (selectValue) {
-          setInputValue('');
-          setSelectedItems((prevState) => {
-            const allSelected = [...prevState, selectValue];
-            onChange(allSelected);
-
-            return allSelected;
-          });
+          handleSelection(selectValue);
         }
       }}
     >
@@ -52,14 +78,6 @@ export default function CreatorSelect({ input, meta, placeholder }) {
           name: input.name,
           placeholder,
         });
-
-        const handleDelete = selectedCreatorId => () => {
-          setSelectedItems((prevState) => {
-            const newSelectedItems = prevState.filter(i => i.id !== selectedCreatorId);
-            onChange(newSelectedItems);
-            return newSelectedItems;
-          });
-        };
 
         return (
           <div>
@@ -77,26 +95,16 @@ export default function CreatorSelect({ input, meta, placeholder }) {
             <div {...getMenuProps()}>
               {isOpen && (
                 <Paper square>
-                  {items.length > 0 ? (
-                    items.map((item, index) => (
-                      <MenuItem
-                        key={item.id}
-                        {...getItemProps({ item })}
-                        selected={highlightedIndex === index}
-                        component="div"
-                      >
-                        {item.name}
-                      </MenuItem>
-                    ))
-                  ) : (
+                  {items.map((item, index) => (
                     <MenuItem
-                      {...getItemProps({ item: NO_RESULTS })}
-                      // selected={highlightedIndex === index}
+                      key={item.id}
+                      {...getItemProps({ item })}
+                      selected={highlightedIndex === index}
                       component="div"
                     >
-                      No results -- click here to add a creator
+                      {item.name}
                     </MenuItem>
-                  )}
+                  ))}
                 </Paper>
               )}
             </div>
