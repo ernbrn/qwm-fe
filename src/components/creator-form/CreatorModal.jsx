3fdd7de2111c2f4cc5ac100d76d5bc57f778e00c
Erin Brown <emberbrown@gmail.com>
2019-07-17 23:25:35 -0400

    Refactor CreatorModal to use FormModal

--- src/components/creator-form/CreatorModal.jsx
+++ src/components/creator-form/CreatorModal.jsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Dialog, DialogTitle, DialogContent, Slide, Grid,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import FormModal from 'shared/multi-select/FormModal';
 import CreatorForm from './CreatorForm';
 
-const useStyles = makeStyles(({ spacing }) => ({
-  content: {
-    padding: spacing(4),
-  },
-}));
-
-const Transition = React.forwardRef((props, ref) => <Slide direction="up" ref={ref} {...props} />);
-
 export default function CreatorModal({ open, handleClose, onSubmit }) {
-  const classes = useStyles();
-
   return (
-    <Dialog fullWidth open={open} onClose={handleClose} TransitionComponent={Transition}>
-      <DialogTitle>Add a creator</DialogTitle>
-      <DialogContent className={classes.content}>
-        <Grid container spacing={4} justify="center">
-          <Grid item>
-            <CreatorForm onSubmit={onSubmit} onCancel={handleClose} />
-          </Grid>
-        </Grid>
-      </DialogContent>
-    </Dialog>
+    <FormModal
+      open={open}
+      handleClose={handleClose}
+      onSubmit={onSubmit}
+      Form={() => <CreatorForm onSubmit={onSubmit} onCancel={handleClose} />}
+      title="Add a creator"
+    />
   );
 }
 
