ddd9dac14a0563b8a4201630092f72e70d781e06
Erin Brown <emberbrown@gmail.com>
2019-08-20 23:25:38 -0400

    Save creator on submit

--- src/components/pages/AddCreator.jsx
+++ src/components/pages/AddCreator.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import CreatorForm from 'creator-form/CreatorForm';
 import { Card, CardContent } from '@material-ui/core';
 import WorkSearch from 'work-form/WorkSelect';
+import { postCreators } from 'creators/creators.service';
 
 export default function AddCreator() {
   function onSubmit(data) {
-    console.log(data);
+    return postCreators(data);
   }
 
   return (
