5d49213281416e17fa7dce93d4ca1e95fded6010
Erin Brown <emberbrown@gmail.com>
2020-04-05 15:11:11 -0400

    Make header text link back to homepage

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -3,6 +3,7 @@ import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import LoginModal from 'modals/LoginModal';
 import LogOutButton from 'shared/LogOutButton';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles(() => ({
   title: {
@@ -16,7 +17,11 @@ export default function HeaderBar() {
   return (
     <AppBar color="default">
       <Toolbar>
-        <Typography className={classes.title}>Queer Works Map</Typography>
+        <Typography className={classes.title}>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            Queer Works Map
+          </Link>
+        </Typography>
         <LogOutButton />
         <LoginModal />
       </Toolbar>
